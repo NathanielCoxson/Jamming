65e7f514c9720fe059245a2b7e93e7c1635082d7
Nathan Coxson <nathancoxson2739@gmail.com>
2022-10-24 00:32:59 -0400

    App.js now passes down searchResults to child components

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {TrackList} from '../TrackList/TrackList.js';
+import './SearchResults.css';
 
 export class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList />
+                <TrackList tracks={this.props.searchResults}/>
             </div>
         );
     }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Track from '../Track/Track.js';
+import {Track} from '../Track/Track.js';
+import './TrackList.css';
 
 export class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                You will add a map method that renders a set of Track components
+                {
+                    this.props.tracks?.map(track => {
+                        return <Track track={track} key={track.id} />
+                    })
+                }
             </div>
         )
     }

0690ef81a5bf1897505cc867fe69e45d5e02874f
Nathan Coxson <nathancoxson2739@gmail.com>
2022-10-24 00:49:36 -0400

    tracks can now be added to the playlist from the search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,8 +10,22 @@ class App extends React.Component {
     this.state = {
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}],
       playlistName: 'playlist',
-      playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}]
+      playlistTracks: []
     };
+    
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({
+      playlistTracks: tracks
+    });
   }
 
   render() {
@@ -21,7 +35,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {TrackList} from '../TrackList/TrackList.js';
+import './Playlist.css'
 
 export class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                <TrackList playlistTracks={this.props.playlistTracks}/>
+                <TrackList tracks={this.props.playlistTracks}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         );  
